25b1fbf4e95be20c9a904706bf16f0dc6dfc3bf0
Ben <benlee1218@gmail.com>
2020-03-07 15:59:21 -0800

    Implemented Basic Semantics

--- lifelang.hs
+++ lifelang.hs
@@ -5,44 +5,53 @@ import           Data.List
 type Pos = Int
 type Health = Int
 type Stamina = Int
-type Lethal = Int
-
-type Prog = [Cmd]
 
 type HState = (Pos, Health, Stamina)
-type OState = (Pos, Lethal)
 
 data Result = OK HState
             | Dead Pos
+            | Error
+            | B Bool
         deriving(Eq, Show)
 
-data Cmd = Jump
-         | Rest
-         | Eat
-         | Damage
-    deriving(Eq,Show)
 
 data Exp = Dec HState
          | Rest Exp Int
          | Eat Exp Int
          | Damage Exp Int
-         | IsAlive Exp
+         | HasStamina Exp
          | If Exp Exp Exp
     deriving(Eq,Show)
 
 
 
+sem :: Exp -> Result
+sem (Dec hstate) = OK hstate
+sem (Rest e v) = case (sem e) of
+                   (OK (pos, health, stamina)) -> (OK (pos, health, stamina+v))
+                   _ -> Error
+sem (Eat e v) = case (sem e) of
+                   (OK (pos, health, stamina)) -> (OK (pos, health+v, stamina))
+                   _ -> Error
+sem (Damage e v) = case (sem e) of
+                   (OK (pos, health, stamina)) -> if (health-v) <= 0 then (Dead pos) else OK (pos, health-v, stamina)
+                   _ -> Error
+sem (HasStamina e) = case (sem e) of
+                   (OK (pos, health, stamina)) -> B (stamina <= 0)
+                   _ -> Error
+sem (If e1 e2 e3) = case (sem e1) of
+                      B True -> sem e2
+                      B False -> sem e3
+                      _ -> Error
 
-cmd :: Cmd -> HState -> Result
-cmd Jump    (p, h, s)     = OK (p+2, h, s-10)
-cmd Jump    (p, h, 0)     = Dead p
-cmd Rest    (p, h, s)     = OK (p, h+10, s+5)
-cmd Eat     (p, h, s)     = OK (p, h+10, s+5)
-cmd Damage  (p, h, s)     = OK (p, h-10, s)
-cmd Damage  (p, 0, s)     = Dead p
+-- good example:
 
-prog :: Prog -> HState -> Result
-prog []     s = OK s
-prog (c:cs) s = case cmd c s of
-                OK s' -> prog cs s'
-                Dead s' -> Dead s'
+
+ex1 :: Exp
+ex1 = Rest (Dec (0, 0, 0)) 10
+-- Should return OK (0, 0, 10)
+
+-- bad example:
+ex4 :: Exp
+ex4 = If (Dec (0, 0, 0)) (Damage (Dec (0, 0, 0)) 3) (Eat (Dec (0, 0, 0)) 3)
+-- Should return Error
