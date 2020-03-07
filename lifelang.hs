8e915160da181e22f90a3ba99a612320c0d66407
Mark Huynh <mark.huynh.oregon@gmail.com>
2020-03-07 15:13:34 -0800

    Inital commit

--- lifelang.hs
+++ lifelang.hs
@@ -22,6 +22,17 @@ data Cmd = Jump
          | Damage
     deriving(Eq,Show)
 
+data Exp = Dec HState
+         | Rest Exp Int
+         | Eat Exp Int
+         | Damage Exp Int
+         | IsAlive Exp
+         | If Exp Exp Exp
+    deriving(Eq,Show)
+
+
+
+
 cmd :: Cmd -> HState -> Result
 cmd Jump    (p, h, s)     = OK (p+2, h, s-10)
 cmd Jump    (p, h, 0)     = Dead p
