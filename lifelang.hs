6f4cc6442109acc07d390d229ea2b45895201b20
Brandon <withingb@oregonstate.edu>
2020-03-07 15:07:57 -0800

    Added alive type and several others
    Implemented alive type so that CMD could return boolean statements. Might work might not. It compiles though.

--- lifelang.hs
+++ lifelang.hs
@@ -2,14 +2,15 @@ module Lifelang where
 
 import           Data.List
 
-type Pos = Int
-type Health = Int
+type Pos     = Int
+type Health  = Int
 type Stamina = Int
-type Lethal = Int
+type Lethal  = Int
+type Alive   = Bool
 
 type Prog = [Cmd]
 
-type HState = (Pos, Health, Stamina)
+type HState = (Pos, Health, Stamina, Alive)
 type OState = (Pos, Lethal)
 
 data Result = OK HState
@@ -17,19 +18,26 @@ data Result = OK HState
         deriving(Eq, Show)
 
 data Cmd = Jump
+         | Walk
          | Rest
          | Eat
          | Damage
+         | IsAlive
     deriving(Eq,Show)
 
 cmd :: Cmd -> HState -> Result
-cmd Jump    (p, h, s)     = OK (p+2, h, s-10)
-cmd Jump    (p, h, 0)     = OK (p+2, h-10, 0)
-cmd Jump    (p, 0, _)     = Dead p
-cmd Rest    (p, h, s)     = OK (p, h+10, s+5)
-cmd Eat     (p, h, s)     = OK (p, h+10, s+5)
-cmd Damage  (p, h, s)     = OK (p, h-10, s)
-cmd Damage  (p, 0, s)     = Dead p
+cmd Walk    (p, h, s, True)     = OK (p+1, h, s-1, True)
+cmd Walk    (p, h, 0, True)     = OK (p+1, h-1, 0, True)
+cmd Walk    (p, h, 0, False)     = Dead p
+cmd Jump    (p, h, s, True)     = OK (p+2, h, s-10, True)
+cmd Jump    (p, h, 0, True)     = OK (p+2, h-10, 0, True)
+cmd Jump    (p, 0, _, False)     = Dead p
+cmd Rest    (p, h, s, True)     = OK (p, h+10, s+5, True)
+cmd Eat     (p, h, s, True)     = OK (p, h+10, s+5, True)
+cmd Damage  (p, h, s, True)     = OK (p, h-10, s, True)
+cmd Damage  (p, 0, s, False)     = Dead p
+cmd IsAlive (p, h, s, True)     = OK (p, h, s, True)
+cmd IsAlive (p, h, s, False)    = Dead p
 
 prog :: Prog -> HState -> Result
 prog []     s = OK s
