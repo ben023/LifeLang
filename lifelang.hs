8df420898a45cf6d66a859f816e152b5e02e9735
Mark Huynh <mark.huynh.oregon@gmail.com>
2020-03-09 17:20:43 -0700

    Implemented basic semantics

--- lifelang.hs
+++ lifelang.hs
@@ -8,50 +8,40 @@ type Stamina = Int
 
 type HState = (Pos, Health, Stamina)
 
-data Result = OK HState
-            | Dead Pos
-            | Error
-            | B Bool
-        deriving(Eq, Show)
+type Var = String
+
+data Expr = Lit Int            --Integer Literal
+            | State HState      --HState
+            | IsAlive Expr     --Boolean if health is not 0
+            | Walk Expr
+            | Rest Expr Expr
+            | Ref Var
+  deriving (Eq,Show)
+
+data Stmt = Bind Var Expr
+            | While Expr Stmt
+            | Block [Stmt]
+  deriving (Eq,Show)
+
+data Type = TInt | TBool | THState
+  deriving (Eq,Show)
+
+type Decl = (Var,Type)
+data Prog = P [Decl] Stmt
+  deriving (Eq,Show)
 
 
-data Exp = Dec HState
-         | Rest Exp Int
-         | Eat Exp Int
-         | Damage Exp Int
-         | HasStamina Exp
-         | If Exp Exp Exp
-    deriving(Eq,Show)
+
+exTemplate :: Prog
+exTemplate = P [("restTime", TInt), ("startingState", THState)]
+            (Block [
+                Bind "restTime" (Lit 4),
+                Bind "startingState" (State (0, 100, 100)),
+                While (IsAlive(Ref "startingState")) 
+                (Block [
+                    Bind "startingState" (Walk (Ref "startingState")),
+                    Bind "startingState" (Rest (Ref "startingState") (Ref "restTime"))
+                ])
+            ])
 
 
-
-sem :: Exp -> Result
-sem (Dec hstate) = OK hstate
-sem (Rest e v) = case (sem e) of
-                   (OK (pos, health, stamina)) -> (OK (pos, health, stamina+v))
-                   _ -> Error
-sem (Eat e v) = case (sem e) of
-                   (OK (pos, health, stamina)) -> (OK (pos, health+v, stamina))
-                   _ -> Error
-sem (Damage e v) = case (sem e) of
-                   (OK (pos, health, stamina)) -> if (health-v) <= 0 then (Dead pos) else OK (pos, health-v, stamina)
-                   _ -> Error
-sem (HasStamina e) = case (sem e) of
-                   (OK (pos, health, stamina)) -> B (stamina <= 0)
-                   _ -> Error
-sem (If e1 e2 e3) = case (sem e1) of
-                      B True -> sem e2
-                      B False -> sem e3
-                      _ -> Error
-
--- good example:
-
-
-ex1 :: Exp
-ex1 = Rest (Dec (0, 0, 0)) 10
--- Should return OK (0, 0, 10)
-
--- bad example:
-ex4 :: Exp
-ex4 = If (Dec (0, 0, 0)) (Damage (Dec (0, 0, 0)) 3) (Eat (Dec (0, 0, 0)) 3)
--- Should return Error
